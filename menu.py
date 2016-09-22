56c63f29f5c3d0371c5f64338a4455a958a037f9
NormanHu <450382690@qq.com>
2016-09-23 01:00:43 +0800

    23

--- menu.py
+++ menu.py
@@ -2,12 +2,25 @@
 #filename:menu.py
 
 import urllib
+import urllib2
+import json
 
 class Menu(object):
+    appId = wx78c9a94295d1c4dd
+    appSecret = 7d3c02a2e4395ef8a2000187b5f277af
+
   def __init__(self):
     pass
+    
+  def getAccessToken(self):
+    access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appId + '&secret=' + secret
+    f = urllib2.urlopen(access_token_url)
+    jsonString = f.read()
+    access_token = json.loads(jsonString)['access_token']
+    return access_token
+
   def create(self,postData,accessToken):
-    postUrl = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s' %accessToken
+    postUrl = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s' % accessToken
     if isinstance(postData,unicode):
       postData = postData.encode('utf-8')
     urlResp = urllib.urlopen(url = postUrl,data = postData)
